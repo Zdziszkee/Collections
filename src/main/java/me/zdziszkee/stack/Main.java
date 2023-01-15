4fec3d7ad3d2239102c984faddaf52311a5b2f37
kkuchta <kkuchta@griddynamics.com>
2023-01-15 22:28:29 +0100

    improved removing efficiency

--- src/main/java/me/zdziszkee/stack/Main.java
+++ src/main/java/me/zdziszkee/stack/Main.java
@@ -1,6 +1,7 @@
 package me.zdziszkee.stack;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -11,6 +12,9 @@ public class Main {
         map.put("3","4");
         map.put("4","5");
         System.out.println(map.get("1"));
+        map.remove("1");
+        System.out.println(map.get("1"));
+
     }
 
 }
--- src/main/java/me/zdziszkee/stack/Map.java
+++ src/main/java/me/zdziszkee/stack/Map.java
@@ -7,17 +7,36 @@ public class Map<K, V> {
     public V put(K key, V value) {
         final int index = key.hashCode() & (entries.length - 1);
         final Node<K, V> currentValue = entries[index];
-        final Node<K, V> next = new Node<>(key, value, null);
+        final Node<K, V> next = new Node<>(key, value, null, currentValue);
         if (currentValue == null) {
             entries[index] = next;
         } else {
             Node<K, V> lastNode = currentValue;
-            while (lastNode.next != null) {
+            do {
+                if (lastNode.key.equals(key)) {
+                    if (value == null) {
+                        final Node<K, V> previousNode = lastNode.previous;
+                        final Node<K, V> nextNode = lastNode.next;
+                        if (previousNode != null) {
+                            previousNode.next = nextNode;
+                        }
+                        if (nextNode != null) {
+                            nextNode.previous = previousNode;
+                        }
+                    } else {
+                        lastNode.value = value;
+                        return value;
+                    }
+                }
                 lastNode = lastNode.next;
-            }
+            } while (lastNode.next != null);
             lastNode.next = next;
         }
         return value;
+    }
+
+    public boolean contains(K key) {
+        return get(key) != null;
     }
 
     public V get(K key) {
@@ -47,10 +66,13 @@ public class Map<K, V> {
 
         Node<K, V> next;
 
-        Node(K key, V value, Node<K, V> next) {
+        Node<K, V> previous;
+
+        Node(K key, V value, Node<K, V> next, Node<K, V> previous) {
             this.key = key;
             this.value = value;
             this.next = next;
+            this.previous = previous;
         }
 
         public final K getKey() {
